372325d37e1f99471429c417f36822c1b4793c52
Simon Kepplinger <simon.kepplinger@valcome.at>
2025-05-04 15:29:01 +0200

    feat: add telescope support for hidden files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,22 @@
-local builtin = require('telescope.builtin')
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find git files' })
-vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope find buffer' })
+local file_ignore_patterns = { "node_modules", ".git" }
+
+telescope.setup({
+	pickers = {
+		find_files = { hidden = true, file_ignore_patterns = file_ignore_patterns },
+		git_files = { hidden = true, file_ignore_patterns = file_ignore_patterns },
+		live_grep = {
+			file_ignore_patterns = file_ignore_patterns,
+			additional_args = function()
+				return { "--hidden" }
+			end,
+		},
+	},
+})
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Telescope find git files" })
+vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffer" })
