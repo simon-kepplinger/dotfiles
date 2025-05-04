469f6e89b7fade3891f8cff4232a7ebebd2911ba
Simon Kepplinger <simon.kepplinger@valcome.at>
2025-05-04 14:11:11 +0200

    feat: change to more popular zsh-vi-mode plugin

--- zsh/.zshrc.d/10-oh-my-zsh.zsh
+++ zsh/.zshrc.d/10-oh-my-zsh.zsh
@@ -5,10 +5,17 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 plugins=(
   zsh-autosuggestions
   zsh-syntax-highlighting
-  zsh-vim-mode
+  zsh-vi-mode
 )
 
 source "$ZSH/oh-my-zsh.sh"
 
 # p10k theme (after OMZ so its functions exist)
 [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+
+# ---- zsh-vi-mode ----
+# disable the cursor style feature
+ZVM_CURSOR_STYLE_ENABLED=false
+
+# use 'N' in normal mode
+typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='N'
