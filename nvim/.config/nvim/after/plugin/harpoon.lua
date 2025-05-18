5a89d6d6e24389f6e1c63218e0028c48bfa9e34e
Simon Kepplinger <simon.kepplinger@valcome.at>
2025-05-18 15:37:37 +0200

    feat: add proper harpoon keybindings

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -2,11 +2,16 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
--- dummy setup: I will try to find good keymaps during actual using it
-vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>hh", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+vim.keymap.set("n", "<leader>ha", function()
+	harpoon:list():add()
+end)
 
-vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
+-- Meta-1 through Meta-4
+for i = 1, 4 do
+	vim.keymap.set("n", "<M-" .. i .. ">", function()
+		harpoon:list():select(i)
+	end)
+end
