439ad394113afbbe2395b8a5188a4caec81d9422
Simon Kepplinger <simon.kepplinger@valcome.at>
2025-05-29 17:47:27 +0200

    chore: add descriptions to harpoon keymaps

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -9,16 +9,17 @@ return {
 
 		vim.keymap.set("n", "<leader>hh", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
+		end, { desc = "Open harpoon" })
+
 		vim.keymap.set("n", "<leader>ha", function()
 			harpoon:list():add()
-		end)
+		end, { desc = "Add to harpoon" })
 
 		-- Meta-1 through Meta-4
 		for i = 1, 4 do
 			vim.keymap.set("n", "<M-" .. i .. ">", function()
 				harpoon:list():select(i)
-			end)
+			end, { desc = "Select harpoon " .. i })
 		end
 	end,
 }
