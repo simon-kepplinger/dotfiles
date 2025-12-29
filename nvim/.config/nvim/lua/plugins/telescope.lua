551498cbb98e46bf2ba2e168b3ffcd08a842b0e3
Simon Kepplinger <simon.kepplinger@valcome.at>
2025-12-29 21:01:05 +0100

    feat(nvim): tweak telescope settings - support open i splits (C-s for horizontal and C-v for vertical) - add new fS grep search to search through git ignored files - include ignored files in default find files command

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,20 +8,22 @@ return {
 
   opts = {
     defaults = {
-      file_ignore_patterns = { 'node_modules', '%.git' },
+      file_ignore_patterns = { 'node_modules', '%.git/' },
       mappings = {
         i = {
           ['<C-j>'] = actions.move_selection_next,
           ['<C-k>'] = actions.move_selection_previous,
+          ['<C-s>'] = actions.select_horizontal,
         },
         n = {
           ['<C-j>'] = actions.move_selection_next,
           ['<C-k>'] = actions.move_selection_previous,
+          ['<C-s>'] = actions.select_horizontal,
         },
       },
     },
     pickers = {
-      find_files = { hidden = true },
+      find_files = { hidden = true, no_ignore = true },
       git_files = { hidden = true },
       live_grep = {
         additional_args = function()
@@ -45,7 +47,18 @@ return {
     {
       '<leader>fs',
       builtin.live_grep,
-      desc = 'File Search (Grep)',
+      desc = 'Live Grep (git files only)',
+    },
+    {
+      '<leader>fS',
+      function()
+        builtin.live_grep({
+          additional_args = function()
+            return { '--hidden', '--no-ignore' }
+          end,
+        })
+      end,
+      desc = 'Live Grep (all files)',
     },
     {
       '<leader>fb',
