09b6216702eb86008bc9dcc9e79f7eff0405bfd9
Simon Kepplinger <simon.kepplinger@valcome.at>
2025-12-30 12:53:27 +0100

    feat(nvim): change neog git window layout - open neogit as a split, and all other menus as a floating window

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -2,13 +2,31 @@ return {
   'NeogitOrg/neogit',
   lazy = true,
   dependencies = {
-    'nvim-lua/plenary.nvim', -- required
-    'sindrets/diffview.nvim', -- optional
-    'nvim-telescope/telescope.nvim', -- optional
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim',
+    'nvim-telescope/telescope.nvim',
   },
   cmd = 'Neogit',
   opts = {
+    kind = 'split',
+
+    floating = {
+      relative = 'editor',
+      width = 0.95,
+      height = 0.95,
+      style = 'minimal',
+      border = 'rounded',
+    },
+
     log_date_format = '%d %b %Y %H:%M',
+
+    commit_editor = { kind = 'floating' },
+    commit_select_view = { kind = 'floating' },
+    log_view = { kind = 'floating' },
+    reflog_view = { kind = 'floating' },
+    refs_view = { kind = 'floating' },
+    stash = { kind = 'floating' },
+    popup = { kind = 'floating' },
   },
   keys = {
     { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Show Neogit UI' },
