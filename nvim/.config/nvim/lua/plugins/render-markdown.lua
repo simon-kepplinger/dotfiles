d727172bf652ed024801d95696df12b1c5df9934
Simon Kepplinger <simon.kepplinger@valcome.at>
2025-12-31 02:05:31 +0100

    feat(nvim): add keymap to render markdown (render default for llm chat)

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,42 @@
-# used by codecompatnion to render chat markdown
+local palette = require('config.theme.palette')
+
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   ft = { 'markdown', 'codecompanion' },
+
+  opts = {
+    enabled = false,
+    file_types = { 'markdown', 'codecompanion' },
+  },
+
+  keys = {
+    {
+      '<leader>mr',
+      '<cmd>RenderMarkdown buf_toggle<CR>',
+      ft = { 'markdown' },
+      desc = 'Markdown: toggle rendered view (buffer)',
+    },
+  },
+
+  config = function(_, opts)
+    require('render-markdown').setup(opts)
+
+    -- Enable rendering automatically ONLY for CodeCompanion buffers (buffer-local)
+    local function enable_cc()
+      require('render-markdown').buf_enable()
+    end
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'codecompanion',
+      callback = enable_cc,
+    })
+
+    -- If we were loaded *inside* an already-open CodeCompanion buffer, enable immediately
+    if vim.bo.filetype == 'codecompanion' then
+      enable_cc()
+    end
+
+    vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { bg = palette.crust })
+    vim.api.nvim_set_hl(0, 'RenderMarkdownCodeInline', { bg = palette.crust })
+  end,
 }
