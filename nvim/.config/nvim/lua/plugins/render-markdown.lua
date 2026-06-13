0411f233b83188fa4781ab0886cf90ab7eea1534
Simon Kepplinger <simon.kepplinger@valcome.at>
2026-06-13 22:10:19 +0200

    chore: drop code compantion plugin
    just use tmux

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -2,11 +2,11 @@ local palette = require('config.theme.palette')
 
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  ft = { 'markdown', 'codecompanion' },
+  ft = { 'markdown' },
 
   opts = {
     enabled = false,
-    file_types = { 'markdown', 'codecompanion' },
+    file_types = { 'markdown' },
   },
 
   keys = {
@@ -20,21 +20,6 @@ return {
 
   config = function(_, opts)
     require('render-markdown').setup(opts)
-
-    -- Enable rendering automatically ONLY for CodeCompanion buffers (buffer-local)
-    local function enable_cc()
-      require('render-markdown').buf_enable()
-    end
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'codecompanion',
-      callback = enable_cc,
-    })
-
-    -- If we were loaded *inside* an already-open CodeCompanion buffer, enable immediately
-    if vim.bo.filetype == 'codecompanion' then
-      enable_cc()
-    end
 
     vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { bg = palette.crust })
     vim.api.nvim_set_hl(0, 'RenderMarkdownCodeInline', { bg = palette.crust })
